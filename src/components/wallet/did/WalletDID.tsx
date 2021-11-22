a2d8d42a8eff212666b34fe2b465131122049eec
zcomputerwiz <zcomputerwiz@gmail.com>
2021-11-22 11:33:15 -0600

    rename XCH to XCC

--- src/components/wallet/did/WalletDID.tsx
+++ src/components/wallet/did/WalletDID.tsx
@@ -613,7 +613,7 @@ const BalanceCardSubSection = (props) => {
         </Box>
         <Box>
           <Typography variant="subtitle1">
-            {mojo_to_chives_string(props.balance)} TXCH
+            {mojo_to_chives_string(props.balance)} TXCC
           </Typography>
         </Box>
       </Box>
